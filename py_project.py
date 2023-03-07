4b395a54a00aee31097823346b20885d484ccad4
GanapathiAmbore <ganapathiambore@gmail.com>
2023-03-07 07:46:45 +0530

    added project files

---
+++ py_project.py
@@ -0,0 +1,32 @@
+
+#version : 1
+# import requests
+# import json
+# url = 'http://universities.hipolabs.com/search?country=United+States'
+# response = requests.get(url)
+# result = response.json()
+# for each in result:
+#     if each['state-province'] is not None:
+#         file = open('full_data.json','w')
+#         file.write(json.dumps(each,indent=4))
+
+#version : 2
+# import requests
+# import json
+# def home(url):
+#     response = requests.get(url)
+#     result = response.json()
+#     print(result)
+#
+# home('http://universities.hipolabs.com/search?country=United+States')
+
+#version 3
+# import requests
+# import json
+# class Project:
+#     def home(self,url):
+#         response = requests.get(url)
+#         result = response.json()
+#         print(result)
+# proj = Project()
+# proj.home('http://universities.hipolabs.com/search?country=United+States')
